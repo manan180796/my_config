c0e99a57c0e9e790e457a861ea5da18d3aae0918
Manan Sharma <manan180796@gmail.com>
2021-05-02 16:34:25 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
-bash sway_config/install.sh
+### SWAY ###
+# remove previous sway file (if it was link else will give error)
+rm $HOME/.config/sway 
+
+# link sway to current directory
+ln -s $(realpath sway_config) $HOME/.config/sway 
+
+### ALACRITTY ###
+# remove previous sway file (if it was link else will give error)
+rm $HOME/.config/alacritty 
+
+# link sway to current directory
+ln -s $(realpath alacritty) $HOME/.config/alacritty 
+
+### WAYBAR ###
+# remove previous sway file (if it was link else will give error)
+rm $HOME/.config/waybar 
+
+# link sway to current directory
+ln -s $(realpath waybar) $HOME/.config/waybar 

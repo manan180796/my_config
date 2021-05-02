5fe3218622a08cec6d2e3093498336796baaba03
Manan Sharma <manan180796@gmail.com>
2021-05-02 16:56:44 +0530

    generalized the installation

--- install.sh
+++ install.sh
@@ -1,20 +1,7 @@
-### SWAY ###
-# remove previous sway file (if it was link else will give error)
-rm $HOME/.config/sway 
 
-# link sway to current directory
-ln -s $(realpath sway) $HOME/.config/sway 
-
-### ALACRITTY ###
-# remove previous sway file (if it was link else will give error)
-rm $HOME/.config/alacritty 
-
-# link sway to current directory
-ln -s $(realpath alacritty) $HOME/.config/alacritty 
-
-### WAYBAR ###
-# remove previous sway file (if it was link else will give error)
-rm $HOME/.config/waybar 
-
-# link sway to current directory
-ln -s $(realpath waybar) $HOME/.config/waybar 
+for file in *; do
+    if [ -d "$file" ]; then
+        rm $HOME/.config/${file}
+        ln -s $(realpath $file) $HOME/.config/${file}
+    fi
+done
